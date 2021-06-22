da0de74ede31c10948c820d8d83ff1b9d3294297
kl <datesaver1234@gmail.com>
2021-06-22 21:46:31 +0900

    form

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
 FROM php:apache
-
--- app/index.php
+++ app/index.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta charset="utf-8">
+  <title>PHP</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 
 <body>
-  <h1>HTML</h1>
-  <?php
-  echo '<h1>hoge</h1>';
-  ?>
+  <header>
+    <div class="header-left">HELLO PHP</div>
+  </header>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form action="url" method="post">
+        <div class="form_item_wrapper">
+          <h4>Email</h4>
+          <input type="text" name="email">
+        </div>
+        <div class="form_item_wrapper">
+          <h4>内容</h4>
+          <textarea name="content"></textarea>
+        </div>
+      </form>
+    </div>
+  </div>
+
 </body>
 
 </html>
